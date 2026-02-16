886b490b0e6aca4cdc3b27b0b9a050fa2e93eafd
soloiaros <soloiaros@gmail.com>
2026-02-16 16:52:33 +0300

    Practice 3.1 - loop the skewed rects in animation Here, I tried to myself implement an infinite loop of rects moving across the screen at a specified angle and speed

--- sketch-skew/trig-skew.js
+++ sketch-skew/trig-skew.js
@@ -4,48 +4,75 @@ import random from 'canvas-sketch-util/random'
 
 const settings = {
   dimensions: [ 1080, 1080 ],
+  animate: true,
+  fps: 16,
 };
 
 const sketch = ({ width, height }) => {
 
-  let x, y, w, h, degrees, rectNum, rects;
+  let rectNum;
   rectNum = 20;
-  rects = [];
+  let rects = [];
   for (let i = 0; i < rectNum; i++) {
-    w = random.range(200, 600);
-    h = random.range(40, 200);
-    x = random.range(0, width);
-    y = random.range(0, height);
-    degrees = random.pick([-30, 150]);
-    rects.push({ x, y, w, h, degrees })
+    rects.push(createRectObject(-200, -40, width + 200, height + 40 ));
   }
 
-  return ({ context, width, height }) => {
+  const moveDistance = 5;
+  const moveAngle = -30;
+  const moveOffsetX = Math.cos(degToRad(moveAngle)) * moveDistance;
+  const moveOffsetY = Math.sin(degToRad(moveAngle)) * moveDistance;
+
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.strokeStyle = 'blue';
+    context.fillStyle = 'blue';
 
-    
-  context.strokeStyle = 'blue';
-  context.fillStyle = 'blue';
-  for (let { x, y, w, h, degrees } of rects) {
-    context.save();
-    context.translate(x, y);
-    drawSkewedRect({ context, w, h, degrees });
-    console.log(x, y)
-    if (90 <= degrees  && degrees <= 270) {context.fill()} else {context.stroke()};
-    context.restore();
-  }
+    if (frame % 2 == 0) {
+      let updatedRects = [];
+      for (let rect of rects) {
+        rect.x -= moveOffsetX;
+        rect.y -= moveOffsetY;
+        if (!(rect.x + rect.rx < 0 || rect.y + rect.ry > height)) {
+          updatedRects.push(rect);
+        } else {
+          let spawnMode = random.pick(['h', 'v']);
+          let widthStart = spawnMode === 'h' ? 0 : width;
+          let heightFinish = spawnMode === 'v' ?  height : -100;
+          updatedRects.push(createRectObject(widthStart, -250, width + 100, heightFinish));
+        }
+      }
+      rects = updatedRects;
+
+    }
+    for (let rect of rects) {
+      context.save();
+      context.translate(rect.x, rect.y);
+      drawSkewedRect({ context, w: rect.w, h: rect.h, rx: rect.rx, ry: rect.ry, degrees: rect.degrees });
+      if (rect.style === 'fill') {context.fill()} else {context.stroke()};
+      context.restore();
+    }
     
   };
 };
 
-function drawSkewedRect({ context, w = 600, h = 200, degrees = 45 }) {
-  const angle = degToRad(degrees);
-  const rx = Math.cos(angle) * w;
-  const ry = Math.sin(angle) * w;
+function createRectObject(xStart, yStart, xFinish, yFinish) {
+  let x, y, w, h, rx, ry, degrees, angle, style;
+  w = random.range(200, 600);
+  h = random.range(40, 200);
+  x = random.range(xStart, xFinish);
+  y = random.range(yStart, yFinish);
+  degrees = -30;
+  angle = degToRad(degrees);
+  rx = Math.cos(angle) * w;
+  ry = Math.sin(angle) * w;
+  style = Math.random() > 0.7 ? 'fill' : 'stroke';
+  return { x, y, rx, ry, w, h, degrees, style };
+}
+
+function drawSkewedRect({ context, w, h, rx, ry }) {
   
   context.save();
-  context.translate(rx * -0.5, (ry + h) * -0.5);
   context.beginPath();
   context.moveTo(0, 0);
   context.lineTo(rx, ry);
