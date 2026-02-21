bffa48ea18d309918060c9b6e46a263a396e71c9
soloiaros <soloiaros@gmail.com>
2026-02-21 21:18:20 +0300

    Curves practice 2 - add mouse dragging The canvas was made interactive by allowing the dragging of the curve points with the cursor

--- curves/quadratic-curve.js
+++ curves/quadratic-curve.js
@@ -1,16 +1,23 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
-const sketch = () => {
+let scaledCanvas, points;
 
-  const points = [
+const sketch = ({ canvas }) => {
+  
+  points = [
     new Point({ x: 200, y: 540 }),
     new Point({ x: 800, y: 800, control: true }),
     new Point({ x: 880, y: 540 }),
   ]
+
+  canvas.addEventListener('mousedown', onMouseDown);
+
+  scaledCanvas = canvas;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -28,6 +35,40 @@ const sketch = () => {
   };
 };
 
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  const x = e.offsetX / scaledCanvas.offsetWidth * scaledCanvas.width;
+  const y = e.offsetY / scaledCanvas.offsetHeight * scaledCanvas.height;
+
+  points.forEach(point => {
+    if (point.hitTest(x, y)) { point.isPressed = true; }
+  })
+}
+
+const onMouseMove = (e) => {
+
+  const x = e.offsetX / scaledCanvas.offsetWidth * scaledCanvas.width;
+  const y = e.offsetY / scaledCanvas.offsetHeight * scaledCanvas.height;
+  
+  points.forEach(point => {
+    if (point.isPressed) {
+      point.x = x;
+      point.y = y
+    }
+  })
+}
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+
+  points.forEach(point => {
+    point.isPressed = false;
+  })
+}
+
 class Point {
   constructor({ x, y, control = false }) {
     this.x = x;
@@ -44,6 +85,13 @@ class Point {
     context.fill();
     context.closePath();
     context.restore();
+  }
+
+  hitTest(x, y) {
+    const dx = this.x - x;
+    const dy = this.y - y;
+    const dd = Math.sqrt(dx ** 2 + dy ** 2);
+    return dd < 20;
   }
 }
 
