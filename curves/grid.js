d79c2f37c44e61fbc3e40b3905998b41ebe89f16
soloiaros <soloiaros@gmail.com>
2026-02-22 16:26:56 +0300

    Curves practice 4 - creating grids with noise I tried making grids using the techniques from the previous practices, but now applying an element of randomness to them with the noise2D method of the random module.

---
+++ curves/grid.js
@@ -0,0 +1,86 @@
+import canvasSketch from 'canvas-sketch';
+import random from 'canvas-sketch-util/random';
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = ({ width, height }) => {
+
+  const numRows = 8;
+  const numCols = 12;
+  const gridWidth = width * 0.8;
+  const gridHeight = height * 0.8;
+  const cellWidth = gridWidth / numCols;
+  const cellHeight = gridHeight / numRows;
+  const gridMarginX = (width - gridWidth) * 0.5;
+  const gridMarginY = (height - gridHeight) * 0.5;
+  // configuring noise
+    const frequency = 0.02;
+    const amplitude = 45;
+    
+    const points = [];
+    for (let r = 0; r < numRows; r++) {
+      for (let c = 0; c < numCols; c++) {
+        let x = gridMarginX + c * cellWidth;
+        let y = gridMarginY + r * cellHeight;
+        const noise = random.noise2D(x, y, frequency, amplitude);
+        x += noise;
+        y += noise;
+      points.push(new Point({ x, y }))
+    }
+  }
+
+
+  
+  return ({ context, width, height }) => {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+    
+    context.save();
+    context.translate(cellWidth * 0.5, cellHeight * 0.5);
+    // drawing dots
+    // for (let point of points) {
+    //   point.draw(context);
+    // }
+    
+    //drawing lines
+    context.strokeStyle = 'red';
+    for (let r = 0; r < numRows; r++) {
+      context.beginPath();
+      for (let c = 0; c < numCols - 1; c++) {
+        const currPoint = points[r * numCols + c + 0];
+        const nextPoint = points[r * numCols + c + 1];
+        const mx = currPoint.x + (nextPoint.x - currPoint.x) * 0.5;
+        const my = currPoint.y + (nextPoint.y - currPoint.y) * 0.5;
+        if (c === 0) context.moveTo(currPoint.x, currPoint.y);
+        else if (c === numCols - 2) context.quadraticCurveTo(currPoint.x, currPoint.y, nextPoint.x, nextPoint.y);
+        else context.quadraticCurveTo(currPoint.x, currPoint.y, mx, my);
+      }
+      context.stroke();
+      context.closePath();
+    }
+    context.restore();
+  };
+};
+
+
+class Point {
+  constructor({ x, y }) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.beginPath();
+    context.arc(0, 0, 10, 0, Math.PI * 2);
+    context.fillStyle = 'red';
+    context.fill();
+    context.closePath();
+    context.restore();
+  }
+}
+
+canvasSketch(sketch, settings);
