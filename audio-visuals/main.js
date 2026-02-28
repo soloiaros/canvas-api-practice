3524aec079b937c0a5a969015c5cfa9ef827c033
soloiaros <soloiaros@gmail.com>
2026-02-28 16:24:26 +0300

    Audio practice 1.1 - make initial setup Everything for the audio to play was added: pause/play on click logic, a pause screen, and a simple view transition

--- audio-visuals/main.js
+++ audio-visuals/main.js
@@ -1,4 +1,26 @@
 import canvasSketch from 'canvas-sketch';
 import { sketch, settings } from './sketch-audio.js';
+import './styles.css'
 
+const audio = document.querySelector('audio');
+audio.autoplay = true;
+
+
+const setPageEventListeners = () => {
+  window.addEventListener('mouseup', () => {
+    document.startViewTransition(() => {
+      const pauseScreen = document.querySelector('.pause-screen');
+      if (pauseScreen.computedStyleMap().get('display').value === 'none') {
+        pauseScreen.style.display = 'block';
+        audio.pause();
+      }
+      else {
+        pauseScreen.style.display = 'none';
+        audio.play();
+      }
+    })
+  })
+}
+
+setPageEventListeners();
 canvasSketch(sketch, settings);
--- audio-visuals/sketch-audio.js
+++ audio-visuals/sketch-audio.js
@@ -6,5 +6,9 @@ export const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.strokeStyle = 'black';
+    context.beginPath();
+    context.arc(500, 500, 400, 0, 360);
+    context.stroke();
   };
 };
